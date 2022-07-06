f9d806adde2d8d8f5015c0f7a2d79f4b31161360
underhazee <uh4zee@gmail.com>
2022-07-06 21:14:30 +0300

    good's data now takes from mockAPI

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import Card from "./components/Card";
 
 import "./scss/app.scss";
 
-import goods from "./assets/goods.json";
-
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://62c5cf49a361f725128f2c86.mockapi.io/items")
+      .then((res) => res.json())
+      .then((json) => setItems(json));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <Categories />
       <h2 className="wrapper__title">Усі комплектуючі</h2>
       <div className="card-flex">
-        {goods.map((obj) => (
+        {items.map((obj) => (
           <Card key={obj.id} {...obj} />
         ))}
       </div>
